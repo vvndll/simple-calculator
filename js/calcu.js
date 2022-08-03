ad9f6dac1776e15fbdedd537a9618684a8862c90
Wendell <vvendell0304@gmail.com>
2022-08-03 17:32:03 +0800

    first attempt

---
+++ js/calcu.js
@@ -0,0 +1,108 @@
+'use strict';
+//Variables
+const displayHistory = document.querySelector(".display__history");
+const displayResult = document.querySelector(".display__result");
+const numberBtn = document.querySelectorAll(".btn__numbers");
+const operatorBtn = document.querySelectorAll(".btn__operator");
+const deleteBtn = document.querySelector(".btn__delete");
+const equalsBtn = document.querySelector(".btn__equals");
+const clearAll = document.querySelector(".btn__clearall")
+const clearLast = document.querySelector(".btn__clearlast")
+
+//Initial Variables for storing output
+let dis1 = "";
+let dis2 = "";
+let result = null;
+let prevOperation = "";
+let putDecimal = false;
+
+//Number Buttons
+numberBtn.forEach(number => {
+    number.addEventListener("click", (e) => {
+        if (e.target.value === "." && !putDecimal) {
+            putDecimal = true;
+        }else if (e.target.value === "." && putDecimal) {
+            return;
+        }
+        dis2 += e.target.value;
+        displayResult.innerText = dis2;
+
+    });
+});
+
+//Operator Buttons
+operatorBtn.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        if (!dis2) return;
+        putDecimal = false;
+
+        const operatorSign = e.target.value;
+
+        if (dis1 && dis2 && prevOperation) {
+            doCalculate();
+        }else {
+            result = parseFloat(dis2);
+        }
+        moveToHistory(operatorSign);
+        prevOperation = operatorSign;
+        
+    });
+});
+
+
+const moveToHistory = (sign = "") => {
+    dis1 += dis2 + " " + sign + " ";
+    displayHistory.innerText = dis1;
+    displayResult.innerText = "";
+    dis2 = "";
+};
+
+
+//Operation function
+const doCalculate = () => {
+    if (prevOperation === "/") {
+        result = parseFloat(result) / parseFloat(dis2);
+    }else if (prevOperation === "x") {
+        result = parseFloat(result) * parseFloat(dis2);
+    }else if (prevOperation === "+") {
+        result = parseFloat(result) + parseFloat(dis2);
+    }else if (prevOperation === "-") {
+        result = parseFloat(result) - parseFloat(dis2);
+    }else if (prevOperation === "%") {
+        result = parseFloat(result) % parseFloat(dis2);
+    }
+};
+
+
+//Outputting Result
+equalsBtn.addEventListener("click", () => {
+    if (!dis2 || !dis1) return;
+    putDecimal = false;
+    doCalculate();
+    moveToHistory();
+    displayResult.innerText = result;
+    dis2 = result;
+    dis1 = "";
+});
+
+//Clear all outputs
+clearAll.addEventListener("click", () => {
+    dis1 = "";
+    dis2 = "";
+    displayHistory.innerText = "";
+    displayResult.innerText = "";
+    result = "";
+});
+
+//Will only clear output in result
+clearLast.addEventListener("click", () => {
+    displayResult.innerText = "";
+    dis2 = "";
+  });
+
+//Delete numbers
+  deleteBtn.addEventListener("click", () => {
+        displayResult.innerText = displayResult.innerText.slice(0, -1);
+        dis2 = dis2.innerText.slice(0, -1);
+
+  })
